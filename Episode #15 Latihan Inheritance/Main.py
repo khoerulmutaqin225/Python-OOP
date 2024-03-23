0a417fa7e5f7d90a8771b5347f38d1e686ac6abf
khoerulmutaqinkm <khoerulmutaqin225@gmail.com>
2024-03-23 11:51:05 +0800

    Episode #15 Latihan Inheritance

---
+++ Episode #15 Latihan Inheritance/Main.py
@@ -0,0 +1,17 @@
+from Hero import Hero_Intelligent, Hero_strength    
+        
+Lina= Hero_Intelligent('Lina')
+Slandar= Hero_strength('Slandar')
+
+Lina.show_info()
+Slandar.show_info()
+
+# Lina.gainExp = 200
+Slandar.gainExp = 200
+        
+
+Lina.show_info()
+Slandar.show_info()
+    
+    
+    
